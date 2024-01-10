fa7cae5346ba84983ea328965245d8089094e5e3
milkmull <mikepmull2.com>
2024-01-09 21:37:50 -0500

    added vector math functions

--- vertex/src/vertex/math/math/detail/common.h
+++ vertex/src/vertex/math/math/detail/common.h
@@ -5,4 +5,19 @@
 #include <type_traits>
 
 #include "../../setup.h"
-#include "type_traits.h"
+
+namespace vx {
+namespace math {
+namespace detail {
+
+using int_float_type = double;
+
+template <typename T, std::enable_if_t<std::is_arithmetic<T>::value, bool> = true>
+struct to_float_type
+{
+    using type = typename std::conditional_t<std::is_integral<T>::value, int_float_type, T>;
+};
+
+}
+}
+}
