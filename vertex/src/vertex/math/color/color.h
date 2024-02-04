bb0d3696e7f22851c85837a4ae38955e9aa57ced
milkmull <mikepmull2.com>
2024-02-04 18:28:33 -0500

    removed some color headers, added color mask enum

--- vertex/src/vertex/math/color/color.h
+++ vertex/src/vertex/math/color/color.h
@@ -2,6 +2,3 @@
 
 #include "detail/colorf_type.h"
 #include "detail/colori_type.h"
-
-#include "color_fn_blend.h"
-#include "color_fn_convert.h"
--- vertex/src/vertex/math/color/color_fn_convert.h
+++ vertex/src/vertex/math/color/color_fn_convert.h
@@ -7,6 +7,14 @@
 namespace vx {
 namespace math {
 
+enum class color_mask
+{
+    R = 0x000000FF,
+    G = 0x0000FF00,
+    B = 0x00FF0000,
+    A = 0xFF000000
+};
+
 // =============== srgb ===============
 
 // https://entropymine.com/imageworsener/srgbformula/
