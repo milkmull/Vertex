c5bde8ac44d5b116f12ea083af18fe625eefc28a
milkmull <mikepmull2.com>
2024-03-16 22:07:39 -0400

    added user friendly includes for rect and color

---
+++ vertex/src/vertex/math/color/color.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "type/color.h"
+
+#include "fn/color_fn_common.h"
+#include "fn/color_fn_comparison.h"
+#include "fn/color_fn_interpolation.h"
---
+++ vertex/src/vertex/math/geometry/rect.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "type/rect.h"
+
+#include "fn/rect_fn_comparison.h"
