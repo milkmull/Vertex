401afe34ea16bb1789dab3d16acc45fdb87b941e
milkmull <mikepmull2.com>
2024-03-17 19:51:22 -0400

    removed extra math file

--- vertex/src/vertex/math/math.h
+++ vertex/src/vertex/math/math.h
@@ -1,15 +1,35 @@
 #pragma once
 
+#include "math/fn/fn_common.h"
+#include "math/fn/fn_comparison.h"
+#include "math/fn/fn_exponential.h"
+#include "math/fn/fn_interpolation.h"
+#include "math/fn/fn_trigonometric.h"
+
+#include "math/fn/vec_fn_common.h"
+#include "math/fn/vec_fn_comparison.h"
+#include "math/fn/vec_fn_exponential.h"
+#include "math/fn/vec_fn_interpolation.h"
+#include "math/fn/vec_fn_geometric.h"
+#include "math/fn/vec_fn_trigonometric.h"
+
 #include "math/type/vec2.h"
 #include "math/type/vec3.h"
 #include "math/type/vec4.h"
 
-#include "math/type/quat.h"
+#include "math/fn/mat_fn_comparison.h"
+#include "math/fn/mat_fn_operations.h"
 
 #include "math/type/mat2x2.h"
 #include "math/type/mat3x3.h"
 #include "math/type/mat4x4.h"
 
-#include "color/type/color.h"
-#include "color/fn/color_fn_comparison.h"
-#include "color/fn/color_fn_interpolation.h"
+#include "math/fn/quat_fn_common.h"
+#include "math/fn/quat_fn_comparison.h"
+#include "math/fn/quat_fn_exponential.h"
+#include "math/fn/quat_fn_interpolation.h"
+#include "math/fn/quat_fn_geometric.h"
+
+#include "math/type/quat.h"
+
+#include "color/color.h"
