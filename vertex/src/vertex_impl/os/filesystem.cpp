f848769bd282bc2cbb02026f2e93a0b50fde4c65
milkmull <mikepmull2.com>
2025-02-12 23:57:09 -0500

    removed absolute call in filesystem equivalent

--- vertex/src/vertex_impl/os/filesystem.cpp
+++ vertex/src/vertex_impl/os/filesystem.cpp
@@ -89,7 +89,7 @@ VX_API path relative(const path& p, const path& base)
 
 VX_API bool equivalent(const path& p1, const path& p2)
 {
-    return equivalent_impl(absolute(p1), absolute(p2));
+    return equivalent_impl(p1, p2);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
